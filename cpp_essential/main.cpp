3ffa53dec53d57a86363d1047ce287e71151f393
imkaywu <imkaywu@gmail.com>
2026-03-30 23:38:36 +0800

    add more examples to template and generic programming

--- cpp_essential/main.cpp
+++ cpp_essential/main.cpp
@@ -8,8 +8,8 @@
 
 #include "memory_management.cpp"
 #include "oop.cpp"
-#include "template_generic_programming.cpp"
 #include "singleton.cpp"
+#include "template_generic_programming.cpp"
 
 using namespace std;
 
@@ -34,8 +34,6 @@ void test_multi_inheritance();
 void test_base_class_init();
 void test_function_template();
 void test_class_template();
-void test_template_specialization();
-void test_variadic_templates();
 void test_nttp();
 void test_crtp();
 void test_concept();
@@ -64,8 +62,6 @@ int main() {
   // test_base_class_init();
   // test_function_template();
   // test_class_template();
-  // test_template_specialization();
-  // test_variadic_templates();
   // test_nttp();
   // test_crtp();
   // test_concept();
@@ -885,78 +881,6 @@ void test_class_template() {
 
   SafeBox<string> safe_box_str("secret");
   cout << safe_box_str.get_value() << endl;
-}
-
-// -----------
-// Template Specialization
-// -----------
-template <typename T1, typename T2>
-class Pair {
- public:
-  void print() { cout << "General pair\n"; }
-};
-
-template <typename T>
-class Pair<T, T> {
- public:
-  void print() { cout << "Partial specialization: both types same\n"; }
-};
-
-void test_template_specialization() {
-  Pair<int, double> p1;
-  Pair<int, int> p2;
-  p1.print();
-  p2.print();
-}
-
-// -----------
-// Variadic templates
-// -----------
-void print() { cout << "No more inputs\n"; }
-
-// ...: defines Args as a pack (like “zero or more types”).
-template <typename T, typename... Args>
-// Args... expands into multiple parameters
-void print(T first, Args... args) {
-  cout << first << " ";
-  // args...: expands into multiple arguments
-  print(args...);
-}
-
-template <typename... Args>
-void print_fold(Args... args) {
-  // comma operator: chain all those operations together inside a fold
-  ((cout << args << " "), ...);
-  cout << "\n";
-}
-
-template <typename... Args>
-void count_args(Args... args) {
-  cout << "Number of args: " << sizeof...(args) << endl;
-}
-
-template <typename T, typename... Args>
-unique_ptr<T> make_unique_custom(Args... args) {
-  return unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
-
-class Person {
- public:
-  Person(string n, int a) { cout << "Constructing " << n << ", " << a << endl; }
-};
-
-void test_variadic_templates() {
-  cout << "=== Expand a parameter pack ===\n";
-  print(1, 2.5, "hello");
-
-  cout << "=== Fold expression ===\n";
-  print_fold(1, 2.5, "hello");
-
-  cout << "=== Count args ===\n";
-  count_args(1, 2.5, "hello");
-
-  cout << "=== Perfect forwarding ===\n";
-  make_unique_custom<Person>("Alice", 10);
 }
 
 // -----------
--- cpp_essential/template_generic_programming.cpp
+++ cpp_essential/template_generic_programming.cpp
@@ -3,7 +3,9 @@
 
 namespace TP {
 
+// ------------
 // Function templates
+// ------------
 template <typename T, typename U>
 T clamp(T value, U low, U high) {
   using Common = decltype(value + low);
@@ -11,7 +13,9 @@ T clamp(T value, U low, U high) {
   return value < low ? low : (value > high ? high : value);
 }
 
+// ------------
 // Class templates
+// ------------
 template <typename T>
 class SimpleVector {
  private:
@@ -49,11 +53,189 @@ class SimpleVector {
   size_t get_size() const { return size; }
 };
 
-int run() {
-  // Function templates
-  clamp(1, 0.0, 2.0);
+// ------------
+// Template specialization
+// ------------
+template <typename T>
+class Printer {
+ public:
+  static void print(const T& value) { std::cout << value << "\n"; }
+};
 
-  // Class templates
+// Full specialization for bool
+template <>
+class Printer<bool> {
+ public:
+  static void print(bool value) {
+    std::cout << (value ? "true" : "false") << "\n";
+  }
+};
+
+// ------------
+// Partial specialization
+// ------------
+template <typename T>
+struct IsPointer {
+  static constexpr bool value = false;
+};
+
+template <typename T>
+struct IsPointer<T*> {
+  static constexpr bool value = true;
+};
+
+template <typename T1, typename T2>
+class Pair {
+ public:
+  void print() { std::cout << "General pair\n"; }
+};
+
+template <typename T>
+class Pair<T, T> {
+ public:
+  void print() { std::cout << "Partial specialization: both types same\n"; }
+};
+
+// -----------
+// Variadic templates
+// https://eli.thegreenplace.net/2014/variadic-templates-in-c/
+// -----------
+void print() {
+  std::cout << __PRETTY_FUNCTION__ << "\n";
+  std::cout << "No more inputs\n";
+}
+
+// typename... Args: template parameter pack
+template <typename T, typename... Args>
+// Args... args: function parameter pack (expands into multiple parameters)
+void print(T first, Args... args) {
+  std::cout << __PRETTY_FUNCTION__ << "\n";
+  std::cout << "First arg: " << first << "\n";
+  // args...: expands into multiple arguments
+  print(args...);
+}
+
+template <typename T>
+bool pair_comparer(T a, T b) {
+  return a == b;
+}
+
+template <typename T>
+bool pair_comparer(T a) {
+  return false;
+}
+
+template <typename T, typename... Args>
+bool pair_comparer(T a, T b, Args... args) {
+  return a == b && pair_comparer(args...);
+}
+
+template <typename... Args>
+void print_fold(Args... args) {
+  // comma operator: chain all those operations together inside a fold
+  ((std::cout << args << " "), ...);
+  std::cout << "\n";
+}
+
+template <typename... Args>
+void count_args(Args... args) {
+  std::cout << "Number of args: " << sizeof...(args) << "\n";
+}
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique_custom(Args&&... args) {
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+class Person {
+ public:
+  Person(std::string n, int a) {
+    std::cout << "Constructing " << n << ", " << a << "\n";
+  }
+};
+
+void test_variadic_templates() {
+  std::cout << "--- Expand a parameter pack ---\n";
+  print(1, 2.5, "hello");
+
+  std::cout << "pair comparer (even): " << pair_comparer(1.5, 1.5, 2, 2, 6, 6)
+            << "\n";
+  std::cout << "pair comparer (odd): " << pair_comparer(1.5, 1.5, 2, 2, 6, 6, 7)
+            << "\n";
+
+  std::cout << "--- Fold expression ---\n";
+  print_fold(1, 2.5, "hello");
+
+  std::cout << "--- Count args ---\n";
+  count_args(1, 2.5, "hello");
+
+  std::cout << "--- Perfect forwarding ---\n";
+  make_unique_custom<Person>("Alice", 10);
+}
+
+// ------------
+// Template type deduction
+// ------------
+// Rule 1: By value -> drops reference & const
+//   template<typename T>
+//   void f(T x);
+//   const int a = 10;
+//   f(a);  // T = int (const dropped)
+// Rule 2: By reference -> preserves const
+//   template<typename T>
+//   void f(T& x);
+//   const int a = 10;
+//   f(a);  // T = const int
+// Rule 3: universal refernce
+//   template<typename T>
+//   void f(T&& x);
+//   int a = 10;
+//   f(a);  // T = int&
+//   f(1);  // T = int
+
+template <typename T>
+void inspect(T&& p) {
+  if constexpr (std::is_lvalue_reference<T>::value) {
+    std::cout << "T is lvalue reference\n";
+  } else {
+    std::cout << "T is NOT lvalue reference\n";
+  }
+}
+
+// ------------
+// decltype
+// ------------
+template <typename T, typename U>
+auto add(T a, U b) -> decltype(a + b) {
+  return a + b;
+}
+
+// ------------
+// enable_if
+// ------------
+// Conditionally enables functions/types at compile time.
+// c++ 11
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value, T>::type square(T x) {
+  return x * x;
+}
+
+// c++ 14
+/*
+template <typename T>
+std::enable_if_t<std::is_integral<T>::value, T> square(T x) {
+  return x * x;
+}
+*/
+
+// ------------
+// run tests
+// ------------
+int run() {
+  std::cout << "=== Function templates ===\n";
+  std::cout << clamp(1, 0.0, 2.0) << "\n";
+
+  std::cout << "=== Class templates ===\n";
   SimpleVector<int> v;
   v.push_back(10);
   v.push_back(20);
@@ -62,6 +244,37 @@ int run() {
   SimpleVector<std::string> vs;
   vs.push_back("hello");
   std::cout << vs[0] << "\n";
+
+  std::cout << "=== Template specialization ===\n";
+  Printer<int>::print(10);
+  Printer<bool>::print(true);
+
+  std::cout << "=== Partial specialization ===\n";
+  std::cout << IsPointer<int>::value << "\n";
+  std::cout << IsPointer<int*>::value << "\n";
+
+  Pair<int, double> p1;
+  Pair<int, int> p2;
+  p1.print();
+  p2.print();
+
+  std::cout << "=== Variadict templates ===\n";
+  test_variadic_templates();
+
+  std::cout << "=== Template type deduction ===\n";
+  int x = 10;
+  inspect(x);   // lvalue -> int&
+  inspect(20);  // rvalue -> int
+
+  std::cout << "=== decltype ===\n";
+  decltype(x) a = 10;   // int
+  decltype((x)) b = x;  // int&
+  b = 50;
+  std::cout << "x: " << x << "\n";
+
+  std::cout << "=== enable_if ===\n";
+  std::cout << "5*5=" << square(5) << "\n";
+  // square(3.14); // ERROR
 
   return 0;
 }
